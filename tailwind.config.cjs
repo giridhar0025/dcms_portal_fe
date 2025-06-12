a6278c75dc7e1f2d8607dd499aa55750de43563b
Giridhar <81301990+giridhar0025@users.noreply.github.com>
2025-06-12 15:46:00 +0530

    fix tailwind apply usage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ const containerQueries = require('@tailwindcss/container-queries')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css}'],
   theme: {
     extend: {
       fontFamily: {
